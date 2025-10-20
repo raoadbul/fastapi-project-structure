4553de43afed8316c7554e1de196655f1dd45f99
Abdul Mateen <abdulmateenrao@gmail.com>
2025-10-20 07:05:16 +0500

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from helpers.lifespan import lifespan
 
 
@@ -19,6 +19,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+@app.post("/media/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    safe_filename = file.filename.replace(" ", "_")
+    with open(f"media/{safe_filename}", "wb") as buffer:
+        buffer.write(await file.read())
+    return {"url": f"/media/{safe_filename}"}
+
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
 app.include_router(user_controller.router, prefix="/api", tags=['User'])
